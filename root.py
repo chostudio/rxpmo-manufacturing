d8848912505f858a0898edfb3c27abc48ee2dbab
Chris H <officialchrisho@gmail.com>
2024-07-01 11:22:58 -1000

    Combine and make new table

--- root.py
+++ root.py
@@ -14,27 +14,41 @@ cur = conn.cursor()
 # Execute a query to create a table
 cur.execute("""
 
-CREATE TABLE ManufacturerProfile (
+CREATE TABLE Manufacturer (
     manufacturer_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     address TEXT NOT NULL,
     naics_code VARCHAR(10),
     product_category VARCHAR(255)
-);
- 
-CREATE TABLE Capabilities (
+
+    -- Capabilities fields
     capability_id SERIAL PRIMARY KEY,
     manufacturer_id INT REFERENCES ManufacturerProfile(manufacturer_id),
     machine_make_model VARCHAR(255) NOT NULL,
     raw_material_used VARCHAR(255) NOT NULL,
     available_tooling VARCHAR(255) NOT NULL,
-);
- 
-CREATE TABLE Capacities (
+
+ -- Capacities fields
     capacity_id SERIAL PRIMARY KEY,
     manufacturer_id INT REFERENCES ManufacturerProfile(manufacturer_id),
     number_of_machines INT NOT NULL,
     utilization_rate NUMERIC(5, 2) NOT NULL,
+);
+
+CREATE TABLE Machine (
+    machine_id SERIAL PRIMARY KEY,
+    manufacturer_id INT REFERENCES Manufacturer(manufacturer_id),
+    type VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    make VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE RawMaterial (
+    material_id SERIAL PRIMARY KEY,
+    manufacturer_id INT REFERENCES Manufacturer(manufacturer_id),
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    density NUMERIC(10, 2) NOT NULL
 );
 
 """)
